e68ca05d4107851832354809566a3d153c31864a
Bacem <bacem-92@hotmail.fr>
2017-11-29 19:01:25 +0100

    user update

---
+++ src/WSBundle/Controller/UserController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace WSBundle\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use WSBundle\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * User controller.
+ *
+ */
+class UserController extends Controller
+{
+
+    /**
+     * Creates a new user entity.
+     *
+     */
+    public function newAction(Request $request)
+    {
+        $data=json_decode($request->getContent(),true);
+        $values = array();
+        $errors = array();
+
+        $em=$this->getDoctrine()->getManager();
+
+        $user = new User();
+
+        if ($request->isMethod('POST')) {
+            $jsonuser=$data;
+            $email = $jsonuser['email'];
+            $firstname = $jsonuser['firstname'];
+            $lastname = $jsonuser['lastname'];
+
+            $values['email'] = $email;
+            $values['firstname'] = $firstname;
+            $values['lastname'] = $lastname;
+
+            $user->setEmail($email);
+            $user->setFirstName($firstname);
+            $user->setLastName($lastname);
+
+            if (count($errors) == 0) {
+
+                $em->persist($user);
+                $em->flush();
+            }
+            return new JsonResponse(array("type"=>"success",'errors' => $errors));
+
+
+        }
+        return new JsonResponse(array("type"=>"failed"));
+
+    }
+}
